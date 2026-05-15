5170b76b28d42939dd77d1f3c73ea91ab667eb98
Kacper Grabowski <kacper.grabowski@globallogic.com>
2026-05-15 13:54:33 +0200

    feat(infra): complete AKS and ACR setup with Terraform
    - Created Azure Resource Group, VNet, and Subnet
    
    - Deployed AKS cluster (Standard_B2s_v2) with OIDC and Workload Identity
    
    - Deployed Azure Container Registry (Basic SKU)
    
    - Configured RBAC for AKS to pull images from ACR
    
    - Added useful Terraform outputs for cluster connection

---
+++ terraform/outputs.tf
@@ -0,0 +1,15 @@
+output "resource_group_name" {
+    value = azurerm_resource_group.main-rg.name
+}
+
+output "kubernetes_cluster_name" {
+    value = azurerm_kubernetes_cluster.aks.name
+}
+
+output "acr_login_server" {
+    value = azurerm_container_registry.acr.login_server
+}
+
+output "aks_config_command" {
+  value = "az aks get-credentials --resource-group ${azurerm_resource_group.main-rg.name} --name ${azurerm_kubernetes_cluster.aks.name}"
+}
---
+++ terraform/variables.tf
@@ -0,0 +1,4 @@
+variable "location" {
+  description = "Location of Azure resources"
+  default = "polandcentral"
+}
